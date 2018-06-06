2ac7ae7a93c36b54040b9e611f59e40915a768fe
Randall Robinson <keith_adams007@yahoo.com>
2018-06-06 19:02:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -24,7 +24,7 @@ class SongsController < ApplicationController
   end
 
   def upload
-    SOngsWorker.perform_async(params[:songs].path)
+    SongsWorker.perform_async(params[:songs].path)
     redirect_to songs_path
   end
 
